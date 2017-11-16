c65c97ee133e4b5c77ebb69ff47cc57f641eb673
Shahrukh Shikalgar <shaharukhs@gmail.com>
2017-11-16 11:13:17 +0530

    Update PostServiceFireBase.swift

--- fireBaseUploadImage/Model/FireBaseServices/PostServiceFireBase.swift
+++ fireBaseUploadImage/Model/FireBaseServices/PostServiceFireBase.swift
@@ -12,25 +12,6 @@ import FirebaseStorage
 
 
 struct PostServiceFireBase {
-//	static func create(for image: UIImage) {
-//		let currentDateTime = Date()
-//		let formatter = DateFormatter()
-//		formatter.dateFormat = "yyyyMMddHH:mm"
-//		let currentDateTimeString = formatter.string(from: currentDateTime)
-//
-//		let filePath = "UserProfile/userPhoto-\(currentDateTimeString)"
-//
-//		let imageRef = Storage.storage().reference().child(filePath)
-//		StorageServiceFireBase.uploadImage(image, at: imageRef) { (downloadURL) in
-//			guard let downloadURL = downloadURL else {
-//				print("Download url not found")
-//				return
-//			}
-//			let urlString = downloadURL.absoluteString
-//			print("image url: \(urlString)")
-//		}
-//	}
-	
 	static func create(for image: UIImage, completion: @escaping (String?) -> ()) {
 		let currentDateTime = Date()
 		let formatter = DateFormatter()
